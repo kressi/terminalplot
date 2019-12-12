5e61e1fad03ab0a40621ff3d4d7ca11214d3d8e7
Michael Kressibucher <mkressibucher@hotmail.com>
2019-12-12 14:00:15 +0100

    Add build number to version if deployed to test

--- setup.py
+++ setup.py
@@ -2,22 +2,29 @@ import setuptools
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
-version_path = os.path.join(here, 'terminalplot', 'version.py')
-readme_path = os.path.join(here, 'README.rst')
 
+version_path = os.path.join(here, 'terminalplot', 'version.py')
 with open(version_path, encoding='utf-8') as f:
     exec(f.read())
 
+readme_path = os.path.join(here, 'README.rst')
 with open(readme_path, encoding='utf-8') as f:
     readme = f.read()
 
+
+def version():
+    if os.getenv('TRAVIS_BUILD_STAGE_NAME', '') == 'Deploy to test':
+        return __version__ + '.' + os.getenv('TRAVIS_BUILD_NUMBER', '')
+    else
+        return __version__
+
 setuptools.setup(
     name='terminalplot',
-    version=__version__ + '.dev' + os.getenv('TRAVIS_BUILD_NUMBER', ''),
+    version=version(),
     description='Plot points in terminal',
     long_description=readme,
     url='https://github.com/kressi/terminalplot',
-    download_url='https://github.com/kressi/terminalplot/tarball/v'+__version__,
+    download_url='https://github.com/kressi/terminalplot/tarball/v' + __version__,
     author='Michael Kressibucher',
     author_email='michael.kressibucher@gmail.com',
     license='GPL',
